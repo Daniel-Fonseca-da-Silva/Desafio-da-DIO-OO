f1f662985f34b6f3214d6e00de65fd1e0ed12ab9
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-03-08 15:46:20 -0300

    Abstração e encapsulamento

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Spring Boot");
+        curso1.setDescricao("Java, hibernate e Spring");
+        curso1.setCargaHoraria(10);
+
+        System.out.println(curso1);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de java e spring");
+        mentoria.setDescricao("Mentoria do curso de java e spring boot");
+        mentoria.setData(LocalDate.now());
+
+        Curso curso2 = new Curso();
+        curso1.setTitulo("Golang");
+        curso1.setDescricao("Introdução ao Go a linguagem do google");
+        curso1.setCargaHoraria(6);
+
+        System.out.println(curso1);
+        System.out.println(mentoria);
+    }
+
+}
