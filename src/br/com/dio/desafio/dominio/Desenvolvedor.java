67c1e5d39ddf704c5a58ecbb55a1b9f3173819e9
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-03-08 18:56:41 -0300

    Criação das classes do dominio

--- src/br/com/dio/desafio/dominio/Desenvolvedor.java
+++ src/br/com/dio/desafio/dominio/Desenvolvedor.java
@@ -2,6 +2,7 @@ package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Desenvolvedor {
@@ -11,14 +12,25 @@ public class Desenvolvedor {
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
     public void inscreverBootcamp(Bootcamp bootcamp) {
-
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDesenvolvedoresInscritos().add(this);
     }
 
     public void progredir() {
-
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if(conteudo.isPresent()) {
+            this.conteudosInscritos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        } else {
+            System.err.println("Você não se matriculou em nenhum conteúdo!");
+        }
     }
 
-    public void calcularTotalXp() {
+    public double calcularTotalXp() {
+        this.conteudosInscritos
+                .stream()
+                .mapToDouble(Conteudo::calcularXp)
+                .sum();
 
     }
 
